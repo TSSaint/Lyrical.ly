f48ed4df02af5b77187e163fc63b8b9cc45327d2
Thomas Santos <thomas@socialeffort.co>
2017-09-24 13:28:15 -0400

    add user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,32 @@
-user.js
+const db = require('../db/config');
+
+// creates an instance of user
+const User = {};
+
+// add method findByUserName
+User.findByUserName = userName => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1
+  `, [userName]);
+};
+
+// add method create
+User.create = user => {
+  return db.one(`
+    INSERT INTO users
+    (username, email. password_digest)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `, [user.username, user.email, user.password_digest]);
+};
+
+// add method findUserSongs
+User.findUserSongs = id => {
+  return db.manyOrNone(`
+    SELECT * FROM songs
+    WHERE user_id = $1
+  `, [id]);
+};
+
+module.exports = User;
