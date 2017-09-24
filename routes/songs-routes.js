40dbe2e04619b24140649a0b30fe703c6c767a05
Thomas Santos <thomas@socialeffort.co>
2017-09-24 12:23:02 -0400

    add songsroutes

---
+++ routes/songs-routes.js
@@ -0,0 +1,16 @@
+// require express
+// initialize songs
+
+const express = require('express');
+const songsRouter = express.Routes();
+
+// helpers
+const songsHelper = require('../services/songs/songs-helpers');
+const songsController = require('../controllers/songs-controller');
+
+// add URI paths
+songsRouter.get('/', songsHelper.getResults, songsController.index);
+songsRouter.get('/:id', songsHelper.getSingle, songsController.show);
+
+module.exports = songsRouter;
+
